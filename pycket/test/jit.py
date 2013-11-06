9f6b78980fe297faaf07e817cb1a4eea0cf03e0d
Carl Friedrich Bolz <cfbolz@gmx.de>
2013-11-06 10:09:39 -0500

    add two more tests

--- pycket/test/jit.py
+++ pycket/test/jit.py
@@ -20,9 +20,48 @@ from pycket.values import *
 
 class TestLLtype(LLJitMixin):
 
-    def test_reverse(self):
+    def test_countdown(self):
 
         ast = to_ast(expand("(letrec ([countdown (lambda (n) (if (< n 0) 1 (countdown (- n 1))))]) (countdown 1000))"))
+
+        def interp_w():
+            val = interpret_one(ast)
+            assert isinstance(val, W_Fixnum)
+            return val.value
+
+        assert interp_w() == 1
+
+        self.meta_interp(interp_w, [], listcomp=True, listops=True, backendopt=True)
+
+    def test_setbang(self):
+
+        ast = to_ast(expand("(let ([n 1000]) (letrec ([countdown (lambda () (if (< n 0) 1 (begin (set! n (- n 1)) (countdown))))]) (countdown)))"))
+
+        def interp_w():
+            val = interpret_one(ast)
+            assert isinstance(val, W_Fixnum)
+            return val.value
+
+        assert interp_w() == 1
+
+        self.meta_interp(interp_w, [], listcomp=True, listops=True, backendopt=True)
+
+    def test_ycombinator(self):
+
+        Y = """
+      (lambda (f)
+        ((lambda (x) (x x))
+         (lambda (g)
+           (f (lambda (z) ((g g) z))))))
+    """
+        countdown = """
+        (lambda (f)
+          (lambda (x)
+            (if (< x 0)
+                1
+                (f (- x 1)))))
+     """
+        ast = to_ast(expand("((%s %s) 1000)"%(Y,countdown)))
 
         def interp_w():
             val = interpret_one(ast)
