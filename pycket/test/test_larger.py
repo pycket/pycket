cfde242d4c9aca080569c01047bedd0eadce3779
Carl Friedrich Bolz <cfbolz@gmx.de>
2013-11-27 15:27:05 +0100

    run the longer tests in reasonable times
    (slightly hacky, string substitution on the loop counters)

--- pycket/test/test_larger.py
+++ pycket/test/test_larger.py
@@ -8,19 +8,22 @@ from pycket.test.test_basic import run_top
 
 current_dir = os.path.dirname(__file__)
 
-def run_file(fname):
+def run_file(fname, *replacements):
     with file(os.path.join(current_dir, fname)) as f:
         s = f.read()
+    for replace, with_ in replacements:
+        assert s.count(replace) == 1
+        s = s.replace(replace, with_)
     return run_top(s)
 
 def test_puzzle():
-    run_file("puzzle.sch")
+    run_file("puzzle.sch", ("1048575", "460"), ("50", "1"))
 
 def test_nqueens():
-    run_file("nqueens.sch")
+    run_file("nqueens.sch", ("10000", "1"))
 
-def test_pseudoknot():
-    run_file("nucleic2.sch")
+#def test_pseudoknot():
+#    run_file("nucleic2.sch")
 
 def test_microkanren():
     run_file("microkanren.sch")
