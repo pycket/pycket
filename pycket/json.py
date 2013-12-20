1cc632b733d10d981d32f5cb2d2cb8fa5194c973
Anton Gulenko <anton.gulenko@googlemail.com>
2013-12-20 16:47:13 +0100

    Tried to fix json.py by avoiding to call non-rpython code in rlib. Still not working.

--- pycket/json.py
+++ pycket/json.py
@@ -1,6 +1,6 @@
 
 from rpython.rlib.parsing.ebnfparse import parse_ebnf, make_parse_function
-from rpython.rlib.parsing.tree import RPythonVisitor
+from rpython.rlib.parsing.tree import RPythonVisitor, Symbol, Nonterminal
 
 _json_grammar = """
     STRING: "\\"[^\\\\"]*\\"";
@@ -22,26 +22,46 @@ def _to_num(s):
     except ValueError:
         return float(s)
 
+# Workaround, because the visit() methods in rlib.parsing.tree are not RPython
+def _do_visit(node, visitor):
+    if isinstance(node, Symbol):
+        if node.symbol == "STRING":
+            return visitor.visit_STRING(node)
+        elif node.symbol == "NUMBER":
+            return visitor.visit_NUMBER(node)
+    if isinstance(node, Nonterminal):
+        if node.symbol == "value":
+            return visitor.visit_value(node)
+        elif node.symbol == "object":
+            return visitor.visit_object(node)
+        elif node.symbol == "array":
+            return visitor.visit_array(node)
+        elif node.symbol == "entry":
+            return visitor.visit_entry(node)
+    assert 0, "Unexpected kind of AST node or symbol"
+
 class ToObjectVisitor(RPythonVisitor):
     def general_visit(self, o):
         assert 0, "All visitor cases should be covered"
     def visit_array(self, arr):
-        return [c.visit(self) for c in arr.children]
+        return [_do_visit(c, self) for c in arr.children]
     def visit_object(self, obj):
-        return dict([entry.visit(self) for entry in obj.children])
+        return dict([_do_visit(entry, self) for entry in obj.children])
     def visit_entry(self, entry):
-        return (entry.children[0].visit(self), entry.children[1].visit(self))
+        return (_do_visit(entry.children[0], self), _do_visit(entry.children[1], self))
     def visit_STRING(self, string):
         s = string.token.source
-        return s[1:len(s)-1] # Strip the " characters
+        return s[1:-1] # Strip the " characters
     def visit_NUMBER(self, num):
         return _to_num(num.token.source)
     def general_symbol_visit(self, sym):
-        if sym.token.source == "null":
-            return None
-        elif sym.token.source == "true":
+        s = sym.token.source
+        assert isinstance(s, str)
+        if s == "null":
+            assert 0, "null not supported in rpython"
+        elif s == "true":
             return True
-        elif sym.token.source == "false":
+        elif s == "false":
             return False
         assert 0, "unexpected kind of symbol: %s"%sym.token.source
 
@@ -51,5 +71,5 @@ def parse_ast(string):
 
 def loads(string):
     ast = parse_ast(string)
-    obj = ast.visit(ToObjectVisitor())
+    obj = _do_visit(ast, ToObjectVisitor())
     return obj
