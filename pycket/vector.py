a7ec18b1d2ea874e7a05fa6a0f45f2741ba8f89a
Carl Friedrich Bolz <cfbolz@gmx.de>
2014-01-20 23:46:38 +0100

    some improvements in vector.py
    (mostly cosmetic, but also make sure that the vector storage is not
    resizable)

--- pycket/vector.py
+++ pycket/vector.py
@@ -2,6 +2,7 @@
 from pycket.values import W_Object, W_Fixnum
 from rpython.rlib import rerased
 from rpython.rlib.objectmodel import newlist_hint
+from rpython.rlib import debug
 
 # Setting this to True will break the tests. Used to compare performance.
 _always_use_object_strategy = False
@@ -39,7 +40,7 @@ class W_Vector(W_Object):
         return W_Vector(strategy, storage)
     def ref(self, i):
         return self.strategy.ref(self, i)
-    def set(self, i, v): 
+    def set(self, i, v):
         self.strategy.set(self, i, v)
     def length(self):
         return self.strategy.length(self)
@@ -56,15 +57,15 @@ class W_Vector(W_Object):
         self.storage = new_strategy.create_storage_for_elements(old_list)
 
 class VectorStrategy(object):
-    def isCorrectType(self, w_obj):
+    def is_correct_type(self, w_obj):
         raise NotImplementedError("abstract base class")
-    
+
     def ref(self, w_vector, i):
         self.indexcheck(w_vector, i)
         return self._ref(w_vector, i)
     def set(self, w_vector, i, w_val):
         self.indexcheck(w_vector, i)
-        if not (self.isCorrectType(w_val)):
+        if not (self.is_correct_type(w_val)):
             self.dehomogenize(w_vector)
             w_vector.set(i, w_val) # Now, try again.
         else:
@@ -79,26 +80,28 @@ class VectorStrategy(object):
         raise NotImplementedError("abstract base class")
     def ref_all(self, w_vector):
         raise NotImplementedError("abstract base class")
-    
+
     def create_storage_for_element(self, element, times):
         raise NotImplementedError("abstract base class")
     def create_storage_for_elements(self, elements):
         raise NotImplementedError("abstract base class")
-    
+
     def dehomogenize(self, w_vector):
         w_vector.change_strategy(ObjectVectorStrategy())
 
 class ObjectVectorStrategy(VectorStrategy):
-    
+
     erase, unerase = rerased.new_erasing_pair("object-vector-strategry")
     erase = staticmethod(erase)
     unerase = staticmethod(unerase)
-    
-    def isCorrectType(self, w_obj):
+
+    def is_correct_type(self, w_obj):
         return True
-    
+
     def _storage(self, w_vector):
-        return self.unerase(w_vector.storage)
+        l = self.unerase(w_vector.storage)
+        debug.make_sure_not_resized(l)
+        return l
     def _ref(self, w_vector, i):
         return self._storage(w_vector)[i]
     def _set(self, w_vector, i, w_val):
@@ -109,62 +112,61 @@ class ObjectVectorStrategy(VectorStrategy):
         assert 0 <= i < len(self._storage(w_vector))
     def ref_all(self, w_vector):
         return self._storage(w_vector)
-    
+
     def create_storage_for_element(self, element, times):
         return self.erase([element] * times)
     def create_storage_for_elements(self, elements):
         return self.erase(elements)
 
     def dehomogenize(self, w_vector):
-        # Already using object strategy. This will not be executed due to isCorrectType().
+        # Already using object strategy. This will not be executed due to is_correct_type().
         pass
 
 class FixnumVectorStrategy(VectorStrategy):
-    
+
     erase, unerase = rerased.new_erasing_pair("fixnum-vector-strategry")
     erase = staticmethod(erase)
     unerase = staticmethod(unerase)
-    
-    def isCorrectType(self, w_obj):
+
+    def is_correct_type(self, w_obj):
         return isinstance(w_obj, W_Fixnum)
-    
+
     def wrap(self, val):
         # TODO what primitive datatype is represented by Fixnum?
         assert isinstance(val, int)
         return W_Fixnum(val)
-    
+
     def unwrap(self, w_val):
         assert isinstance(w_val, W_Fixnum)
         return w_val.value
-    
+
     def _storage(self, w_vector):
-        return self.unerase(w_vector.storage)
-    
+        l = self.unerase(w_vector.storage)
+        debug.make_sure_not_resized(l)
+        return l
+
     def _ref(self, w_vector, i):
         return self.wrap(self._storage(w_vector)[i])
-    
+
     def _set(self, w_vector, i, w_val):
         self._storage(w_vector)[i] = self.unwrap(w_val)
-    
+
     def length(self, w_vector):
         return len(self._storage(w_vector))
-    
+
     def indexcheck(self, w_vector, i):
         assert 0 <= i < len(self._storage(w_vector))
-    
+
     def ref_all(self, w_vector):
         unwrapped = self._storage(w_vector)
         return [self.wrap(i) for i in unwrapped]
 
     def create_storage_for_element(self, element, times):
-        l = newlist_hint(times)
         e = self.unwrap(element)
-        for i in range(times):
-            l.append(e)
-        return self.erase(l)
-    
+        return self.erase([e] * times)
+
     def create_storage_for_elements(self, elements):
-        l = newlist_hint(len(elements))
+        l = [0] * len(elements)
         for i in range(len(elements)):
-            l.append(self.unwrap(elements[i]))
+            l[i] = self.unwrap(elements[i])
         return self.erase(l)
