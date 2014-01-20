1f9d1f4056986f8c70a90052ab6f1003791d4a32
Carl Friedrich Bolz <cfbolz@gmx.de>
2014-01-20 23:20:58 +0100

    reformat vector_iterate.rkt
    (not really sure whether this is more idiomatic)

--- pycket/test/vector_iterate.rkt
+++ pycket/test/vector_iterate.rkt
@@ -1,14 +1,21 @@
 #lang racket
-(letrec(
-    [elements 1000000]
-    [loops 20]
-    [vec (make-vector elements 2)]
-    [do-loop-vec (lambda(n) (if (< n 0) vec 
-        (begin
-            (vector-set! vec n (* 3 (vector-ref vec n)))
-            (do-loop-vec (sub1 n)))))]
-    [loop-vec (lambda () (do-loop-vec (sub1 elements)))]
-    [repeat-times (lambda (n f) (if (= n 0) (f) (begin (f) (repeat-times (sub1 n) f))))])
+
+(letrec([elements 1000000]
+        [loops 20]
+        [vec (make-vector elements 2)]
+        [do-loop-vec
+         (lambda(n)
+           (if (< n 0)
+               vec 
+               (begin
+                (vector-set! vec n (* 3 (vector-ref vec n)))
+                (do-loop-vec (sub1 n)))))]
+        [loop-vec
+         (lambda ()
+           (do-loop-vec (sub1 elements)))]
+        [repeat-times
+         (lambda (n f) 
+           (if (= n 0) (f) (begin (f) (repeat-times (sub1 n) f))))])
 
     (repeat-times loops loop-vec)
 
