a58180dcb1b526f51a536950c8f14156cfe776f8
Carl Friedrich Bolz <cfbolz@gmx.de>
2014-05-01 15:52:39 +0200

    remove a level of indirections in primitive continuations

--- pycket/prims.py
+++ pycket/prims.py
@@ -76,16 +76,33 @@ def continuation(func):
     continuation triggers, the original function is called with one extra
     argument, the computed vals. """
 
+    import inspect
+    argspec = inspect.getargspec(func)
+    assert argspec.varargs is None
+    assert argspec.keywords is None
+    assert argspec.defaults is None
+    argnames = argspec.args[:-1]
+
+    unroll_argnames = unroll.unrolling_iterable(enumerate(argnames))
+
     class PrimCont(Cont):
-        def __init__(self, args):
-            self.args = args
+        _immutable_fields_ = argnames
+
+        def __init__(self, *args):
+            for i, name in unroll_argnames:
+                setattr(self, name, args[i])
 
         def plug_reduce(self, vals):
-            args = self.args + (vals, )
+            args = ()
+            for i, name in unroll_argnames:
+                args += (getattr(self, name), )
+            args += (vals, )
             return func(*args)
     PrimCont.__name__ = func.func_name + "PrimCont"
+
     def make_continuation(*args):
-        return PrimCont(args)
+        return PrimCont(*args)
+
     make_continuation.func_name = func.func_name + "_make_continuation"
     return make_continuation
 
