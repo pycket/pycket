8ee86cb4ddb322171ed5ec2c1f5b3ee9ce184e39
Vasily Kirilichev <vkirilichev@gmail.com>
2014-06-23 16:34:29 +0200

    4-space indent

--- pycket/test/test_struct.py
+++ pycket/test/test_struct.py
@@ -2,86 +2,86 @@ from pycket.test.testhelper import *
 from pycket.values import *
 
 def test_current_inspector(source):
-  """
-  (inspector? (current-inspector))
-  """
-  result = run_mod_expr(source, wrap=True)
-  assert result == w_true
+    """
+    (inspector? (current-inspector))
+    """
+    result = run_mod_expr(source, wrap=True)
+    assert result == w_true
 
 def test_make_struct_type(source):
-  """
-  (define-values (struct:p0 make-p0 p0? p0-ref p0-set!) (make-struct-type 'p0 #f 3 0 #f null 'prefab #f '(0 1 2)))
-  (and 
-    (struct-type? struct:p0) 
-    (struct-constructor-procedure? make-p0) 
-    (struct-predicate-procedure? p0?) 
-    (struct-accessor-procedure? p0-ref) 
-    (struct-mutator-procedure? p0-set!))
-  """
-  result = run_mod_expr(source, wrap=True)
-  assert result == w_true
+    """
+    (define-values (struct:p0 make-p0 p0? p0-ref p0-set!) (make-struct-type 'p0 #f 3 0 #f null 'prefab #f '(0 1 2)))
+    (and 
+      (struct-type? struct:p0) 
+      (struct-constructor-procedure? make-p0) 
+      (struct-predicate-procedure? p0?) 
+      (struct-accessor-procedure? p0-ref) 
+      (struct-mutator-procedure? p0-set!))
+    """
+    result = run_mod_expr(source, wrap=True)
+    assert result == w_true
 
 def test_make_struct_field_accessor(source):
-  """
-  (define-values (struct:p1 make-p1 p1? p1-ref p1-set!) (make-struct-type 'p1 #f 3 0 #f null 'prefab #f '(0 1 2)))
-  (define accessor (make-struct-field-accessor p1-ref 0))
-  (procedure? accessor)
-  """
-  result = run_mod_expr(source, wrap=True)
-  assert result == w_true
+    """
+    (define-values (struct:p1 make-p1 p1? p1-ref p1-set!) (make-struct-type 'p1 #f 3 0 #f null 'prefab #f '(0 1 2)))
+    (define accessor (make-struct-field-accessor p1-ref 0))
+    (procedure? accessor)
+    """
+    result = run_mod_expr(source, wrap=True)
+    assert result == w_true
 
 def test_struct_main_functions(source):
-  """
-  (struct posn (x y))
+    """
+    (struct posn (x y))
 
-  (let* ([p (posn 1 2)]
-         [p? (posn? p)]
-         [x (posn-x p)]
-         [y (posn-y p)])
-  (and p? (= x 1) (= y 2)))
-  """
-  result = run_mod_expr(source, wrap=True)
-  assert result == w_true
+    (let* ([p (posn 1 2)]
+           [p? (posn? p)]
+           [x (posn-x p)]
+           [y (posn-y p)])
+    (and p? (= x 1) (= y 2)))
+    """
+    result = run_mod_expr(source, wrap=True)
+    assert result == w_true
 
 def test_struct_copying_and_update(source):
-  """
-  (struct posn (x y))
+    """
+    (struct posn (x y))
 
-  (let* ([p1 (posn 1 2)]
-         [p2 (struct-copy posn p1 [x 3])]
-         [x1 (posn-x p1)]
-         [x2 (posn-x p2)]
-         [y2 (posn-y p2)])
-  (and (= x1 1) (= x2 3) (= y2 2)))
-  """
-  result = run_mod_expr(source, wrap=True)
-  assert result == w_true
+    (let* ([p1 (posn 1 2)]
+           [p2 (struct-copy posn p1 [x 3])]
+           [x1 (posn-x p1)]
+           [x2 (posn-x p2)]
+           [y2 (posn-y p2)])
+    (and (= x1 1) (= x2 3) (= y2 2)))
+    """
+    result = run_mod_expr(source, wrap=True)
+    assert result == w_true
 
 def test_struct_subtypes(source):
-  """
-  (struct posn (x y))
-  (struct 3d-posn posn (z))
+    """
+    (struct posn (x y))
+    (struct 3d-posn posn (z))
 
-  (let* ([p (3d-posn 1 2 3)]
-         [p? (posn? p)]
-         [x (posn-x p)]
-         [z (3d-posn-z p)])
-  (and p? (= x 1) (= z 3)))
-  """
-  result = run_mod_expr(source, wrap=True)
-  assert result == w_true
+    (let* ([p (3d-posn 1 2 3)]
+           [p? (posn? p)]
+           [x (posn-x p)]
+           [z (3d-posn-z p)])
+    (and p? (= x 1) (= z 3)))
+    """
+    result = run_mod_expr(source, wrap=True)
+    assert result == w_true
 
 def test_struct_comparison(source):
-  """
-  (struct glass (width height) #:transparent)
-  (struct lead (width height))
-  (define slab (lead 1 2))
+    """
+    (struct glass (width height) #:transparent)
+    (struct lead (width height))
+    (define slab (lead 1 2))
 
-  (let* ([glass_test (equal? (glass 1 2) (glass 1 2))]
-         [slab (lead 1 2)]
-         [lead_test1 (equal? slab slab)]
-         [lead_test2 (equal? slab (lead 1 2))])
-  (and glass_test lead_test1 (not lead_test2)))
-  """
-  result = run_mod_expr(source, wrap=True)
-  assert result == w_true
+    (let* ([glass_test (equal? (glass 1 2) (glass 1 2))]
+           [slab (lead 1 2)]
+           [lead_test1 (equal? slab slab)]
+           [lead_test2 (equal? slab (lead 1 2))])
+    (and glass_test lead_test1 (not lead_test2)))
+    """
+    result = run_mod_expr(source, wrap=True)
+    assert result == w_true
