c4c3ac21eb8cb94b650daa6fb7032dc90b5db175
Spenser Bauman <sabauma@gmail.com>
2014-06-29 12:32:34 -0400

    Fixup tests to not reference 'racket/unsafe/ops'

--- pycket/test/test-module.rkt
+++ pycket/test/test-module.rkt
@@ -1,12 +1,13 @@
 #lang pycket #:stdlib
 
 ;;(require (except-in racket/tcp tcp-listen))
-;;(require "pycket/test/imp-vector.rkt")
-;;(require (only-in "pycket/test/imp-vector.rkt" check))
-(require (rename-in "pycket/test/imp-vector.rkt" [check check^]))
+(require "./imp-vector.rkt")
+(require (only-in "./imp-vector.rkt" check))
+;;(require data/queue)
+;;(require (rename-in "pycket/test/imp-vector.rkt" [check check^]))
 ;;(require racket/unsafe/ops)
 ;;(require (prefix-in check "pycket/test/imp-vector.rkt"))
 
-(define k check^)
-(displayln k)
+;;(define k check)
+;;(displayln k)
 
