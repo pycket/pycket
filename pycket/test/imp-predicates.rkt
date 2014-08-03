a8f04a517e6a2c62dd5829da4482f330ff76fb37
Spenser Bauman <sabauma@gmail.com>
2014-08-03 18:22:22 -0400

    Add chaperone/impersonator predicate tests that will actually fail

--- pycket/test/imp-predicates.rkt
+++ pycket/test/imp-predicates.rkt
@@ -59,11 +59,21 @@
     (lambda (a b) b)
     (lambda (a b) b)))
 
+(define assert
+  (lambda (v)
+    (unless v
+      (error 'imp-predicates "Assertion violation"))))
+
 (printf "~nchaperone-of? procedure examples~n")
 (printf "test-proc ? imp-proc  => ~s = #f ~n" (chaperone-of? test-proc imp-proc))
 (printf "imp-proc  ? test-proc => ~s = #f ~n" (chaperone-of? imp-proc test-proc))
 (printf "chp-proc  ? test-proc => ~s = #t ~n" (chaperone-of? chp-proc test-proc))
 (printf "test-proc ? chp-proc  => ~s = #f ~n" (chaperone-of? test-proc chp-proc))
+
+(assert (not (chaperone-of? test-proc imp-proc)))
+(assert (not (chaperone-of? imp-proc test-proc)))
+(assert (chaperone-of? chp-proc test-proc))
+(assert (not (chaperone-of? test-proc chp-proc)))
 
 (printf "~nimpersonator-of? procedure examples~n")
 (printf "test-proc ? chp-proc  => ~s = #f ~n" (impersonator-of? test-proc chp-proc))
@@ -73,11 +83,23 @@
 (printf "imp-proc  ? sub1      => ~s = #f ~n" (impersonator-of? imp-proc sub1))
 (printf "sub1      ? imp-proc  => ~s = #f ~n" (impersonator-of? sub1 imp-proc))
 
+(assert (not (impersonator-of? test-proc chp-proc)))
+(assert (not (impersonator-of? test-proc imp-proc)))
+(assert (impersonator-of? chp-proc test-proc))
+(assert (impersonator-of? imp-proc test-proc))
+(assert (not (impersonator-of? imp-proc sub1)))
+(assert (not (impersonator-of? sub1 imp-proc)))
+
 (printf "~nchaperone-of? vector examples~n")
 (printf "test-vector ? imp-vector  => ~s = #f ~n" (chaperone-of? test-vector imp-vector))
 (printf "imp-vector  ? test-vector => ~s = #f ~n" (chaperone-of? imp-vector test-vector))
 (printf "chp-vector  ? test-vector => ~s = #t ~n" (chaperone-of? chp-vector test-vector))
 (printf "test-vector ? chp-vector  => ~s = #f ~n" (chaperone-of? test-vector chp-vector))
+
+(assert (not (chaperone-of? test-vector imp-vector)))
+(assert (not (chaperone-of? imp-vector test-vector)))
+(assert (chaperone-of? chp-vector test-vector))
+(assert (not (chaperone-of? test-vector chp-vector)))
 
 (printf "~nimpersonator-of? vector examples~n")
 (printf "test-vector ? chp-vector  => ~s = #f ~n" (impersonator-of? test-vector chp-vector))
@@ -87,11 +109,23 @@
 (printf "imp-vector  ? sub1        => ~s = #f ~n" (impersonator-of? imp-vector sub1))
 (printf "sub1        ? imp-vector  => ~s = #f ~n" (impersonator-of? sub1 imp-vector))
 
+(assert (not (impersonator-of? test-vector chp-vector)))
+(assert (not (impersonator-of? test-vector imp-vector)))
+(assert (impersonator-of? chp-vector test-vector))
+(assert (impersonator-of? imp-vector test-vector))
+(assert (not (impersonator-of? imp-vector sub1)))
+(assert (not (impersonator-of? sub1 imp-vector)))
+
 (printf "~nequality of vectors with impersonators~n")
 (printf "(equal? test-vector test-vector) => ~s~n" (equal? test-vector test-vector))
 (printf "(equal? test-vector imp-vector)  => ~s~n" (equal? test-vector imp-vector))
 (printf "(equal? test-vector chp-vector)  => ~s~n" (equal? test-vector chp-vector))
 (printf "(equal? chp-vector test-vector)  => ~s~n" (equal? chp-vector test-vector))
+
+(assert (equal? test-vector test-vector))
+(assert (equal? test-vector imp-vector))
+(assert (equal? test-vector chp-vector))
+(assert (equal? chp-vector test-vector))
 
 (printf "~nimpersonator?/chaperone?~n")
 (printf "(chaperone? test-vector)    => ~s = #f~n" (chaperone? test-vector))
@@ -101,16 +135,35 @@
 (printf "(impersonator? chp-vector)  => ~s = #t~n" (impersonator? chp-vector))
 (printf "(impersonator? chp-vector)  => ~s = #t~n" (impersonator? imp-vector))
 
+(assert (not (chaperone? test-vector)))
+(assert (chaperone? chp-vector))
+(assert (not (chaperone? imp-vector)))
+(assert (not (impersonator? test-vector)))
+(assert (impersonator? chp-vector))
+(assert (impersonator? imp-vector))
+
 (printf "~nchaperone-of? box examples~n")
 (printf "test-box ? imp-box  => ~s = #f ~n" (chaperone-of? test-box imp-box))
 (printf "imp-box  ? test-box => ~s = #f ~n" (chaperone-of? imp-box test-box))
 (printf "chp-box  ? test-box => ~s = #t ~n" (chaperone-of? chp-box test-box))
 (printf "test-box ? chp-box  => ~s = #f ~n" (chaperone-of? test-box chp-box))
 
+(assert (not (chaperone-of? test-box imp-box)))
+(assert (not (chaperone-of? imp-box test-box)))
+(assert (chaperone-of? chp-box test-box))
+(assert (not (chaperone-of? test-box chp-box)))
+
 (printf "~nimpersonator-of? box examples~n")
 (printf "test-box ? chp-box  => ~s = #f ~n" (impersonator-of? test-box chp-box))
 (printf "test-box ? imp-box  => ~s = #f ~n" (impersonator-of? test-box imp-box))
 (printf "chp-box  ? test-box => ~s = #t ~n" (impersonator-of? chp-box test-box))
 (printf "imp-box  ? test-box => ~s = #t ~n" (impersonator-of? imp-box test-box))
-(printf "imp-box  ? sub1      => ~s = #f ~n" (impersonator-of? imp-box sub1))
-(printf "sub1      ? imp-box  => ~s = #f ~n" (impersonator-of? sub1 imp-box))
+(printf "imp-box  ? sub1     => ~s = #f ~n" (impersonator-of? imp-box sub1))
+(printf "sub1     ? imp-box  => ~s = #f ~n" (impersonator-of? sub1 imp-box))
+
+(assert (not (impersonator-of? test-box chp-box)))
+(assert (not (impersonator-of? test-box imp-box)))
+(assert (impersonator-of? chp-box test-box))
+(assert (impersonator-of? imp-box test-box))
+(assert (not (impersonator-of? imp-box sub1)))
+(assert (not (impersonator-of? sub1 imp-box)))
