85f092dcfa1d61558b53c96bcc25583ba048d509
Spenser Bauman <sabauma@gmail.com>
2014-08-09 17:29:50 -0400

    Remove unused import

--- pycket/prims.py
+++ pycket/prims.py
@@ -282,7 +282,7 @@ def check_cont(proc, v, v1, v2, env, cont, _vals):
         [values_struct.W_StructType, values.W_Object, procedure,
          values.W_Object, values.W_Object], simple=False)
 def do_checked_procedure_check_and_extract(type, v, proc, v1, v2, env, cont):
-    from interpreter import return_value
+    # FIXME make this impersonator/chaperone friendly
     if isinstance(v, values_struct.W_Struct) and v.struct_type() is type:
         first_field = v._ref(0)
         assert first_field.iscallable()
