1944614d7ac14d54fe98fa4269f51c5e9c0c3f11
Spenser Bauman <sabauma@gmail.com>
2014-09-15 02:41:03 -0400

    AST class needs to be in its own file due to using it in cont.py

---
+++ pycket/AST.py
@@ -0,0 +1,65 @@
+
+class AST(object):
+    _attrs_ = ["should_enter", "mvars", "surrounding_lambda"]
+    _immutable_fields_ = ["should_enter?", "surrounding_lambda"]
+    _settled_ = True
+
+    should_enter = False # default value
+    mvars = None
+    surrounding_lambda = None
+
+    simple = False
+
+    def defined_vars(self): return {}
+
+    def interpret(self, env, cont):
+        from pycket.interpreter import return_value
+        # default implementation for simple AST forms
+        assert self.simple
+        return return_value(self.interpret_simple(env), env, cont)
+
+    def interpret_simple(self, env):
+        raise NotImplementedError("abstract base class")
+
+    def set_surrounding_lambda(self, lam):
+        from pycket.interpreter import Lambda
+        assert isinstance(lam, Lambda)
+        self.surrounding_lambda = lam
+        for child in self.direct_children():
+            child.set_surrounding_lambda(lam)
+
+    def direct_children(self):
+        return []
+
+    def free_vars(self):
+        free_vars = {}
+        for child in self.direct_children():
+            free_vars.update(child.free_vars())
+        return free_vars
+
+    def assign_convert(self, vars, env_structure):
+        """ make a copy of the AST that converts all writable variables into
+        using cells. In addition, compute the state of the environment for
+        every AST node that needs to know.
+
+        The vars argument contains the variables that need to use cells.
+        The env_structure is an instance of SymList (or None) describing the
+        environment at that AST node.
+        """
+        raise NotImplementedError("abstract base class")
+
+    def mutated_vars(self):
+        if self.mvars is not None:
+            return self.mvars
+        self.mvars = self._mutated_vars()
+        return self.mvars
+
+    def _mutated_vars(self):
+        raise NotImplementedError("abstract base class")
+
+    def tostring(self):
+        return "UNKNOWN AST: "
+
+    def __str__(self):
+        return self.tostring()
+
