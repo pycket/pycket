ce994754a62d365cd19686b1cc11dc3a33a51c91
Tobias Pape <tobias@netshed.de>
2014-09-16 13:18:18 +0200

    Revert "A test for a bug."
    This reverts commit 1252ca71469782f7da6ac2901af81ed49ea25f7d.

--- pycket/test/test_larger.py
+++ pycket/test/test_larger.py
@@ -60,10 +60,5 @@ def test_thread_cells():
 def test_impersonator_properties():
     run_file("impersonator-properties.rkt")
 
-@pytest.mark.xfail
-def test_reverse():
-    # might be a regression
-    run_file("test_reverse.rkt")
-
 #def test_minikanren():
 #    run_file("minikanren.sch")
