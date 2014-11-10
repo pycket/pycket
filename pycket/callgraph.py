94ea45f7b937dfd97a2cc08f7b673b00bfa533cb
Carl Friedrich Bolz <cfbolz@gmx.de>
2014-11-10 20:17:00 +0100

    reduce number of dict lookups and calls to is_recursive

--- pycket/callgraph.py
+++ pycket/callgraph.py
@@ -16,20 +16,28 @@ class CallGraph(object):
         subdct = self.calls.get(calling_lam, None)
         if subdct is None:
             self.calls[calling_lam] = subdct = {}
+            lam_in_subdct = False
+        else:
+            lam_in_subdct = lam in subdct
         cont_ast = cont.get_next_executed_ast()
         config = env.pycketconfig()
-        if lam not in subdct:
+        is_recursive = False
+        if not lam_in_subdct:
             subdct[lam] = None
             if self.is_recursive(calling_lam, lam):
+                is_recursive = True
                 if config.log_callgraph:
                     print "enabling jitting", calling_lam.tostring()
                 calling_lam.enable_jitting()
         # It is possible to have multiple consuming continuations for a given
         # function body. This will attempt to mark them all.
         same_lambda = cont_ast and cont_ast.surrounding_lambda is calling_lam
-        if same_lambda and self.is_recursive(calling_lam, lam):
-            if cont_ast.set_should_enter() and config.log_callgraph:
-                print "jitting downrecursion", cont_ast.tostring()
+        if same_lambda:
+            if lam_in_subdct: # did not call is_recursive yet
+                is_recursive = self.is_recursive(calling_lam, lam)
+            if is_recursive:
+                if cont_ast.set_should_enter() and config.log_callgraph:
+                    print "jitting downrecursion", cont_ast.tostring()
 
     def is_recursive(self, lam, starting_from=None):
         # quatratic in theory, hopefully not very bad in practice
