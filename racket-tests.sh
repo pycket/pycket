070ed12dcf0b1e7936c4ee0d8704fb68afd66e88
Vasily Kirilichev <vkirilichev@gmail.com>
2014-11-19 17:08:30 +0100

    add redirecting error output for racket tests script and comment out not supported yet object-name calls

--- racket-tests.sh
+++ racket-tests.sh
@@ -2,10 +2,12 @@
 
 tests=( "pycket/test/struct-test.rkt" )
 
+printf "\n*** Racket Tests ***\n"
+
 for test in "${tests[@]}"
 do
     printf "Running $test.. "
-    result=$(./pycket-c-c-nojit $test | awk '/Errors/{y=1;next;}y')
+    result=$(./pycket-c-c-nojit $test 2>&1 >/dev/null | awk '/Errors/{y=1;next;}y')
     if [ ! -z "$result" -a "$result" != " " ]; then
         printf "failed:\n$result\n"
         exit 1
