32ffdc42c50e1d78a3cd29c2fbe05617aac13a45
Spenser Bauman <sabauman@indiana.edu>
2015-05-07 14:12:35 -0400

    Add better printing of bytes

--- pycket/values.py
+++ pycket/values.py
@@ -817,7 +817,7 @@ class W_Bytes(W_Object):
         make_sure_not_resized(self.value)
 
     def tostring(self):
-        return "#\"%s\"" % "".join(self.value)
+        return "#\"%s\"" % "".join(["\\%d" % ord(i) for i in self.value])
 
     def equal(self, other):
         if not isinstance(other, W_Bytes):
