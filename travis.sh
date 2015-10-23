d1b7ae1db323fd3b6617b6e4fe966864496ecab4
Tobias Pape <tobias@netshed.de>
2015-10-23 21:25:21 +0200

    debug

--- travis.sh
+++ travis.sh
@@ -81,7 +81,7 @@ do_tests() {
 }
 
 do_coverage() {
-  set +e
+  set +ex
   rm -rf ../pypy/*pytest*
   py.test --assert=reinterp -rf -n 3 -k "$COVERAGE_TESTSUITE" --cov . --cov-report=term pycket
   echo '>> Testing whether coverage is over 80%'
