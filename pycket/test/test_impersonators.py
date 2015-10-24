1d3ed3198a6a5a3ff81e30970767bbd72e3c6102
Spenser Bauman <sabauma@gmail.com>
2015-10-24 13:42:50 -0400

    Duplicate tests for impersonators

--- pycket/test/test_impersonators.py
+++ pycket/test/test_impersonators.py
@@ -98,6 +98,80 @@ def test_noninterposing_chaperone():
     assert isinstance(interp, W_InterposeStructBase)
     assert interp.get_property(blue) == sym("color")
 
+def test_noninterposing_impersonator():
+    sym = W_Symbol.make
+    m = run_mod(
+    """
+    #lang pycket
+    (define-values (prop:blue blue? blue-ref) (make-impersonator-property 'blue))
+    (define-values (prop:green green? green-ref) (make-struct-type-property 'green 'can-impersonate))
+    (define a-equal+hash (list
+                         (lambda (v1 v2 equal?)
+                           (equal? (aa-y v1) (aa-y v2)))
+                         (lambda (v1 hash)
+                           (hash (aa-y v1)))
+                         (lambda (v2 hash)
+                           (hash (aa-y v2)))))
+    (define (a-impersonator-of v) (a-x v))
+    (define (aa-y v) (if (a? v) (a-y v) (pre-a-y v)))
+      (define-struct pre-a (x y)
+        #:mutable
+        #:property prop:equal+hash a-equal+hash
+        #:property prop:green 'color)
+      (define-struct a (x y)
+        #:mutable
+        #:property prop:impersonator-of a-impersonator-of
+        #:property prop:equal+hash a-equal+hash)
+      (define-struct (a-more a) (z) #:mutable)
+      (define-struct (a-new-impersonator a) ()
+        #:mutable
+        #:property prop:impersonator-of a-impersonator-of)
+      (define-struct (a-new-equal a) ()
+        #:mutable
+        #:property prop:equal+hash a-equal+hash)
+    (define a-pre-a (impersonate-struct (make-pre-a 17 1) pre-a-y (lambda (a v) v)))
+    (define t1 (impersonator-of? a-pre-a a-pre-a))
+    (define t2
+      (impersonator-of?
+        (make-pre-a 17 1)
+        (impersonate-struct (make-pre-a 17 1) pre-a-y #f prop:blue 'color)))
+    (define t3
+      (impersonator-of?
+        (make-pre-a 17 1)
+        (impersonate-struct a-pre-a pre-a-y #f prop:blue 'color)))
+    (define t4
+      (impersonator-of? a-pre-a
+        (impersonate-struct a-pre-a pre-a-y #f prop:blue 'color)))
+    (define t5
+      (impersonator-of?
+        (impersonate-struct a-pre-a pre-a-y #f prop:blue 'color)
+        a-pre-a))
+    (define t6
+      (impersonator-of?
+        a-pre-a
+        (impersonate-struct a-pre-a pre-a-y (lambda (a v) v) prop:blue 'color)))
+    (define t7
+      (impersonator-of? a-pre-a
+        (impersonate-struct a-pre-a green-ref (lambda (a v) v))))
+    (define non-interposing (impersonate-struct (make-pre-a 17 1) pre-a-y #f prop:blue 'color))
+    """)
+    assert m.defs[sym("t1")] is w_true
+    assert m.defs[sym("t2")] is w_true
+    assert m.defs[sym("t3")] is w_false
+    assert m.defs[sym("t4")] is w_true
+    assert m.defs[sym("t5")] is w_true
+    assert m.defs[sym("t6")] is w_false
+    assert m.defs[sym("t7")] is w_false
+
+    a_pre_a = m.defs[sym("a-pre-a")]
+    assert not a_pre_a.is_non_interposing_chaperone()
+    interp = m.defs[sym("non-interposing")]
+    assert interp.is_non_interposing_chaperone()
+
+    blue = m.defs[sym("prop:blue")]
+    assert isinstance(interp, W_InterposeStructBase)
+    assert interp.get_property(blue) == sym("color")
+
 def test_noninterposing_chaperone_procedure():
     m = run_mod(
     """
@@ -115,6 +189,30 @@ def test_noninterposing_chaperone_procedure():
     (define t3 (chaperone-of? g3 (chaperone-procedure g3 #f prop:blue 'color)))
     (define t4 (chaperone-of? f3 (chaperone-procedure f3 #f prop:blue 'color)))
     (define t5 (chaperone-of? f3 (chaperone-procedure g3 #f prop:blue 'color)))
+    """)
+    assert m.defs[W_Symbol.make("t1")] is values.w_true
+    assert m.defs[W_Symbol.make("t2")] is values.w_true
+    assert m.defs[W_Symbol.make("t3")] is values.w_true
+    assert m.defs[W_Symbol.make("t4")] is values.w_true
+    assert m.defs[W_Symbol.make("t5")] is values.w_false
+
+def test_noninterposing_impersonate_procedure():
+    m = run_mod(
+    """
+    #lang racket/base
+    (define-values (prop:blue blue? blue-ref) (make-impersonator-property 'blue))
+    (define wrapper (lambda (x) x))
+    (define f1 (lambda (a) a))
+    (define f2 (lambda (b) b))
+    (define f3 (lambda (c) c))
+    (define g1 (impersonate-procedure f1 wrapper))
+    (define g2 (impersonate-procedure f2 wrapper))
+    (define g3 (impersonate-procedure f2 wrapper))
+    (define t1 (impersonator-of? g1 (impersonate-procedure g1 #f prop:blue 'color)))
+    (define t2 (impersonator-of? g2 (impersonate-procedure g2 #f prop:blue 'color)))
+    (define t3 (impersonator-of? g3 (impersonate-procedure g3 #f prop:blue 'color)))
+    (define t4 (impersonator-of? f3 (impersonate-procedure f3 #f prop:blue 'color)))
+    (define t5 (impersonator-of? f3 (impersonate-procedure g3 #f prop:blue 'color)))
     """)
     assert m.defs[W_Symbol.make("t1")] is values.w_true
     assert m.defs[W_Symbol.make("t2")] is values.w_true
@@ -155,6 +253,35 @@ def test_chaperone_vector_to_list():
     (define v (vector 1 2 3 4 5))
     (define cell 0)
     (define imp
+      (chaperone-vector v
+        (lambda (self i v) (set! cell (+ cell 1)) v)
+        (lambda (self i v) v)))
+    (define chp
+      (chaperone-vector v
+        (lambda (self i v) (set! cell (+ cell 1)) v)
+        (lambda (self i v) v)))
+    (define base (vector->list v))
+    (define lst1 (vector->list imp))
+    (define lst2 (vector->list chp))
+    (define cmp1 (equal? base lst1))
+    (define cmp2 (equal? base lst2))
+    """)
+    cmp1 = m.defs[W_Symbol.make("cmp1")]
+    cmp2 = m.defs[W_Symbol.make("cmp2")]
+    cell = m.defs[W_Symbol.make("cell")]
+    assert cmp1 is values.w_true
+    assert cmp2 is values.w_true
+    assert isinstance(cell, values.W_Cell)
+    count = cell.get_val()
+    assert isinstance(count, values.W_Fixnum) and count.value == 10
+
+def test_impersonate_vector_to_list():
+    m = run_mod(
+    """
+    #lang pycket
+    (define v (vector 1 2 3 4 5))
+    (define cell 0)
+    (define imp
       (impersonate-vector v
         (lambda (self i v) (set! cell (+ cell 1)) v)
         (lambda (self i v) v)))
@@ -176,4 +303,3 @@ def test_chaperone_vector_to_list():
     assert isinstance(cell, values.W_Cell)
     count = cell.get_val()
     assert isinstance(count, values.W_Fixnum) and count.value == 10
-
