f48c6e313ccc26a2590fd211c321d8693d54db57
Spenser Bauman <sabauma@gmail.com>
2015-11-03 14:35:19 -0500

    Fix type error

--- pycket/prims/input_output.py
+++ pycket/prims/input_output.py
@@ -376,7 +376,8 @@ def file_exists(w_str):
 @expose("directory-list", [values.W_Object])
 def dir_list(w_str):
     s = extract_path(w_str)
-    return values.to_list(os.listdir(s))
+    dir = [values.W_Path(p) for p in os.listdir(s)]
+    return values.to_list(dir)
 
 
 @continuation
@@ -387,10 +388,13 @@ def close_cont(port, env, cont, vals):
 
 def extract_path(obj):
     if isinstance(obj, values_string.W_String):
-        return obj.as_str_utf8()
-    if isinstance(obj, values.W_Path):
-        return obj.path
-    raise SchemeException("expected path-like values but got %s" % obj.tostring())
+        result = obj.as_str_utf8()
+    elif isinstance(obj, values.W_Path):
+        result = obj.path
+    else:
+        raise SchemeException("expected path-like values but got %s" % obj.tostring())
+    return result if result is not None else "."
+
 
 def open_infile(w_str, mode):
     s = extract_path(w_str)
