be4ea8602c0ba23c44dfab09a9fe5339a475d6ec
Spenser Bauman <sabauma@gmail.com>
2016-01-29 18:25:48 -0500

    Source information

--- pycket/vmprof_support.py
+++ pycket/vmprof_support.py
@@ -10,7 +10,10 @@ def _get_code(ast, env):
 vmprof_profile = rvmprof.vmprof_execute_code("pycket", _get_code, result_class=W_Object)
 
 def _get_full_name(ast):
-    return "rkt:func_name:func_line:filename"
+    lam = ast.surrounding_lambda
+    filename = lam.srcfile
+    srcline  = lam.srcpos
+    return "rkt:<lambda>:%s:%s" % (filename, srcline)
 
 rvmprof.register_code_object_class(AST, _get_full_name)
 
