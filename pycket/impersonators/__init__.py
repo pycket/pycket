98b5cc41ac2eab72cd312c3f5af94053c3c0d420
Spenser Bauman <sabauma@gmail.com>
2016-06-08 13:54:37 -0400

    Use hidden classes by default

--- pycket/impersonators/__init__.py
+++ pycket/impersonators/__init__.py
@@ -1,7 +1,7 @@
 
 from pycket import config
 
-if not config.hidden_classes:
+if config.hidden_classes:
     from pycket.impersonators.hidden_classes import *
 else:
     from pycket.impersonators.baseline import *
