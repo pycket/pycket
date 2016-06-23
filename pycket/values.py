9071469e38ee429316c924409f0cc7f2036cc160
Carl Friedrich Bolz <cfbolz@gmx.de>
2016-06-23 09:59:47 +0200

    kill unused decorator (there is an equivalent one in rlib.jit anyway)

--- pycket/values.py
+++ pycket/values.py
@@ -24,18 +24,6 @@ from rpython.rlib.debug import check_list_of_chars, make_sure_not_resized, check
 
 UNROLLING_CUTOFF = 5
 
-def elidable_iff(pred):
-    def wrapper(func):
-        func = jit.unroll_safe(func)
-
-        def trampoline(*args):
-            if jit.we_are_jitted() and pred(*args):
-                return elidable_func(*args)
-            return func(*args)
-
-        return trampoline
-    return wrapper
-
 @inline_small_list(immutable=True, attrname="vals", factoryname="_make")
 class Values(W_ProtoObject):
     def __init__(self):
