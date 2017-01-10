c5903667ef165c3c41ef503787253fc93bbfc0fc
Spenser Bauman <sabauma@gmail.com>
2017-01-10 12:07:11 -0500

    Remove option to log callgraph operations (not really needed now)

--- pycket/callgraph.py
+++ pycket/callgraph.py
@@ -44,18 +44,15 @@ class CallGraph(object):
             subdct[lam] = None
             if self.is_recursive(calling_lam, lam):
                 is_recursive = True
-                if config.log_callgraph:
-                    print "enabling jitting", calling_lam.tostring()
                 calling_lam.enable_jitting()
         # It is possible to have multiple consuming continuations for a given
         # function body. This will attempt to mark them all.
         same_lambda = cont_ast and cont_ast.surrounding_lambda is calling_lam
         if same_lambda:
-            if lam_in_subdct: # did not call is_recursive yet
-                is_recursive = self.is_recursive(calling_lam, lam)
-            if is_recursive:
-                if cont_ast.set_should_enter() and config.log_callgraph:
-                    print "jitting downrecursion", cont_ast.tostring()
+            # did not call is_recursive yet
+            if lam_in_subdct and self.is_recursive(calling_lam, lam):
+                if self.is_recursive(calling_lam, lam):
+                    cont_ast.set_should_enter()
 
     def is_recursive(self, lam, starting_from=None):
         # quatratic in theory, hopefully not very bad in practice
