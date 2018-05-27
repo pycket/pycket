5162deab816cb34d5e5b4d568968522db5b7c4f4
Caner Derici <canerderici@gmail.com>
2018-05-27 13:37:50 -0400

    adding the remaining arithmetic tests

--- pycket/test/test_testing.py
+++ pycket/test/test_testing.py
@@ -15,6 +15,19 @@ def test_doctest(doctest):
     2
     """
 
+@pytest.mark.comp
+def test_string_to_sexp_complex_numbers(doctest):
+    r"""
+    > (/ 2+3i 2)
+    1+3/2i
+    > (/ 2+3i 3-4i)
+    -6/25+17/25i
+    > (/ -2-3i -4+5i)
+    -7/41+22/41i
+    > (= 5+5i 5+5.0i)
+    #t
+    """
+
 @pytest.mark.skipif(not pytest.config.load_expander, reason="quote issue")
 def test_string_to_sexp(doctest):
     r"""
