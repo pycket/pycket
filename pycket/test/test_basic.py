2152147da60db1451e3e793e79548d14703c5c90
Caner Derici <canerderici@gmail.com>
2018-09-14 13:37:01 -0400

    remove the shadowing letrec

--- pycket/test/test_basic.py
+++ pycket/test/test_basic.py
@@ -75,7 +75,6 @@ def test_letrec():
     run_fix("(letrec ([x 1]) x)", 1)
     run_fix("(letrec ([x 1] [y 2]) y)", 2)
     run_fix("(letrec ([x 1] [y 2]) (+ x y))", 3)
-    run_fix("(let ([x 0]) (letrec ([x 1] [y x]) (+ x y)))", 2)
     run_fix("(letrec ([x (lambda (z) x)]) 2)", 2)
 
 def test_reclambda():
