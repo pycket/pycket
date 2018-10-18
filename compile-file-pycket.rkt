e1d24d69396fcba750423682d6e587020aa0f48f
Caner Derici <canerderici@gmail.com>
2018-10-18 12:08:27 -0400

    check timestamps of zo files

--- compile-file-pycket.rkt
+++ compile-file-pycket.rkt
@@ -326,8 +326,11 @@
          [dirs (cdr p-list)]
          [p (apply collection-file-path mod-name dirs)]
          [compiled-file-name (format "~a_rkt.zo" (car p-list))]
-         [zo-path (build-path (path-only p) "pycket-compiled" compiled-file-name)])
-    (if (or force-recompile (not (file-exists? zo-path)))
+         [zo-path (apply collection-file-path (cons compiled-file-name
+                                                    compiled-dir))])
+    (if (or force-recompile
+            (<= (file-or-directory-modify-seconds zo-path #f (lambda () -inf.0))
+                (file-or-directory-modify-seconds p #f)))
         (begin
           (printf "PYCKET COMPILE FILE -- compiling : ~a\n" p)
           (compile-file p))
