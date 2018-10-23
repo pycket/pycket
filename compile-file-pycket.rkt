623efcd062a9fa7d1e4a39b2a8e4cbb30fb1fddf
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2018-10-23 17:15:06 -0400

    Use `compiler/compile-file`.

--- compile-file-pycket.rkt
+++ compile-file-pycket.rkt
@@ -1,68 +1,13 @@
 #lang racket/base
 (require racket/path racket/list
          racket/file
-         racket/cmdline)
+         racket/cmdline
+         syntax/modread
+         (prefix-in c: compiler/compile-file))
 (provide compile-file compile-lib-path)
 
-(define compile-file
-  (case-lambda
-    [(src)
-     (define cdir (build-path (path-only src) "pycket-compiled"))
-     (make-directory* cdir)
-     (compile-file src (build-path cdir (path-add-suffix (file-name-from-path src) #".zo")))]
-    [(src dest)
-     (compile-file src dest values)]
-    [(src dest filter)
-     (define in (open-input-file src))
-     (dynamic-wind
-      void
-      (lambda ()
-        (define ok? #f)
-        ; This must be based on the path to dest. Renaming typically cannot be done
-        ; atomically across file systems, so the temporary directory is not an option
-        ; because it is often a ram disk. src (or dir below) couldn't be used because
-        ; it may be on a different filesystem. Since dest must be a file path, this
-        ; guarantees that the temp file is in the same directory. It would take a weird
-        ; filesystem configuration to break that.
-        (define temp-filename (make-temporary-file "tmp~a" #f (path-only dest)))
-        (port-count-lines! in)
-        (dynamic-wind
-         void
-         (lambda ()
-           ; XXX: This seems like it should be a library function named 'relative-path-only'
-           (define dir
-             (let-values ([(base name dir?) (split-path src)])
-               (if (eq? base 'relative)
-                   (current-directory)
-                   (path->complete-path base (current-directory)))))
-           (define out (open-output-file temp-filename #:exists 'truncate/replace))
-           (parameterize ([current-load-relative-directory dir]
-                          [current-write-relative-directory dir]
-                          [read-accept-reader #t])
-             ; Rather than installing a continuation barrier, we detect reinvocation.
-             ; The only thing that can cause reinvocation is if the filter captures the
-             ; continuation and communicates it externally.
-             (define count 0)
-             (dynamic-wind
-              (lambda ()
-                (if (zero? count)
-                    (set! count 1)
-                    (error 'compile-file "filter function should not be re-entrant")))
-              (lambda ()
-                #;(printf "read-accept-lang : ~a -- read-accept-reader : ~a\n" (read-accept-lang) (read-accept-reader))
-                (for ([r (in-port (lambda (in) (read-syntax src in)) in)])
-                  (write (compile-syntax (filter (namespace-syntax-introduce r))) out))
-                (set! ok? #t))
-              (lambda ()
-                (close-output-port out)))))
-         (lambda ()
-           (if ok?
-               (rename-file-or-directory temp-filename dest #t)
-               (with-handlers ([exn:fail:filesystem? void])
-                 (delete-file temp-filename))))))
-      (lambda () (close-input-port in)))
-
-     (build-path dest)]))
+(define (compile-file src)
+  (with-module-reading-parameterization (lambda () (c:compile-file src))))
 
 (define racket-modules
   '("racket/private/stx"
@@ -139,6 +84,7 @@
     "syntax/readerr"
     "syntax/module-reader"
     "syntax/wrap-modbeg"
+    "syntax/modread"
     "setup/cross-system"
     "setup/dirs"
     "setup/private/dirs"
@@ -313,6 +259,7 @@
     ;; racket/match ??
     ;; "racket/struct" ;; needs contracts
     ;; "syntax/strip-context" ;; needs contracts
+    "compiler/compile-file"
     ))
 
 
@@ -327,7 +274,7 @@
          [p (apply collection-file-path mod-name dirs)]
          [compiled-file-name (format "~a_rkt.zo" (car p-list))]
          [zo-path (build-path (path-only p) "pycket-compiled" compiled-file-name)])
-    (if (or force-recompile
+    (if (or (force-recompile)
             (<= (file-or-directory-modify-seconds zo-path #f (lambda () -inf.0))
                 (file-or-directory-modify-seconds p #f)))
         (begin
@@ -356,41 +303,44 @@
       (when (file-exists? zo-path)
         (delete-file zo-path)))))
 
-;; fake-parameterize
-(define old-ns (current-namespace))
 
-(define batch #f)
-(define clean #f)
-(define force-recompile #f)
-(define lib-path? #f)
-(command-line
- #:once-each
- [("-b" "--batch") "compile the Racket modules statically listed here"
-                   (set! batch #t)]
- [("--clean") "remove all the generated pycket .zo files for racket modules"
-              (set! clean #t)]
- [("-f" "--force") "force re-generation of existing zo files (disabled by default)"
-                   (set! force-recompile #t)]
- [("-l") "interpret paths as library paths"
-         (set! lib-path? #t)]
- #:args paths
- ;; do this with mutation because parameterization doesn't currently work
- (current-namespace (make-base-namespace))
- ;; to do multiple
- (when batch
-   (for ([p (in-list racket-modules)])
-     (compile-lib-path p)))
- 
- ;; to compile individual paths
- (when (not (null? paths))
-   (for ([p (in-list paths)])
-     (if lib-path?
-         (compile-lib-path p)
-         (compile-path p))))
- 
- (when clean
-   (for ([p (in-list racket-modules)])
-     (clean-file p)))
- (printf "DONE.\n"))
+(define force-recompile (make-parameter #f))
 
-(current-namespace old-ns)
+
+;(module+ main
+  (define batch #f)
+  (define clean #f)
+  (define lib-path? #f)
+  ;; fake-parameterize
+  (define old-ns (current-namespace))
+  (command-line
+   #:once-each
+   [("-b" "--batch") "compile the Racket modules statically listed here"
+                     (set! batch #t)]
+   [("--clean") "remove all the generated pycket .zo files for racket modules"
+                (set! clean #t)]
+   [("-f" "--force") "force re-generation of existing zo files (disabled by default)"
+                     (force-recompile #t)]
+   [("-l") "interpret paths as library paths"
+           (set! lib-path? #t)]
+   #:args paths
+   ;; do this with mutation because parameterization doesn't currently work
+   (current-namespace (make-base-namespace))
+   ;; to do multiple
+   (when batch
+     (for ([p (in-list racket-modules)])
+       (compile-lib-path p)))
+   
+   ;; to compile individual paths
+   (when (not (null? paths))
+     (for ([p (in-list paths)])
+       (if lib-path?
+           (compile-lib-path p)
+           (compile-path p))))
+   
+   (when clean
+     (for ([p (in-list racket-modules)])
+       (clean-file p)))
+   (printf "DONE.\n"))
+
+  (current-namespace old-ns);)
