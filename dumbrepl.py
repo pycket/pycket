d6a03fad6c9280981ae3beee24de89bd6361bcc9
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2018-12-13 17:04:23 -0500

    Make sure things are loaded right.

--- dumbrepl.py
+++ dumbrepl.py
@@ -1,4 +1,8 @@
 if __name__ == "__main__":
+    import pycket.values
+    import pycket.config
+    from pycket.env import w_global_config
+    #w_global_config.set_linklet_mode_off()
     import pycket.test.testhelper as th
     th.dumb_repl()
 
