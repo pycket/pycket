048155ddef4eab00235acea68b812d9142d691fb
Caner Derici <canerderici@gmail.com>
2018-12-22 10:18:33 -0500

    removing the breakpoint in the test

--- pycket/test/test_old_entry_point.py
+++ pycket/test/test_old_entry_point.py
@@ -183,7 +183,6 @@ class TestCommandline(object):
         printval = "42"
         assert entry_point(['arg0', '-e', '(display %s)' % printval]) == 0
         out, err = capfd.readouterr()
-        import pdb;pdb.set_trace()
         assert out == "%s" % printval
 
     def test_f(self, capfd, racket_file):
