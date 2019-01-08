e366504ed1b323db127e23ab6a22633534ca7e35
Caner Derici <canerderici@gmail.com>
2019-01-08 13:35:29 -0500

    implement getwidth for some of the regexp classes
    Fixes #262

--- pycket/prims/primitive_tables.py
+++ pycket/prims/primitive_tables.py
@@ -71,6 +71,7 @@ network_str = ["tcp-abandon-port", "tcp-accept", "tcp-accept-evt", "tcp-accept-r
                "udp-multicast-leave-group!", "udp-multicast-loopback?", "udp-multicast-set-interface!", "udp-multicast-set-loopback!",
                "udp-multicast-set-ttl!", "udp-multicast-ttl", "udp-open-socket", "udp-receive!",
                "udp-receive!*", "udp-receive!-evt", "udp-receive!/enable-break", "udp-receive-ready-evt",
+               "udp-set-receive-buffer-size!",
                "udp-send", "udp-send*", "udp-send-evt", "udp-send-ready-evt",
                "udp-send-to", "udp-send-to*", "udp-send-to-evt", "udp-send-to/enable-break",
                "udp-send/enable-break"]
