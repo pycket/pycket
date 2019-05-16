76aafdbde7cbb59f316a3f6d9e7c0ed4012ad5c7
Caner Derici <canerderici@gmail.com>
2019-05-16 15:49:25 -0400

    basic fasling, able to handle symbol integer and lists

--- pycket/fasl.py
+++ pycket/fasl.py
@@ -56,7 +56,90 @@ FASL_SMALL_INTEGER_START = 100
 
 FASL_LOWEST_SMALL_INTEGER = -10
 FASL_HIGHEST_SMALL_INTEGER = 255 - ((FASL_SMALL_INTEGER_START - FASL_LOWEST_SMALL_INTEGER) - 1)
+FASL_PREFIX = "racket/fasl:"
+FASL_PREFIX_LENGTH = len(FASL_PREFIX)
 
 FASL_HASH_EQ_VARIANT = 0
 FASL_HASH_EQUAL_VARIANT = 1
 FASL_HASH_EQV_VARIANT = 2
+
+from rpython.rlib             import streamio as sio
+import os
+
+def fasl_to_sexp_file(file_name):
+    stream = sio.open_file_as_stream(file_name, "rb", buffering=2**21)
+    return fasl_to_sexp(stream)
+
+def fasl_to_sexp(stream):
+
+    prefix = stream.read(FASL_PREFIX_LENGTH)
+    if prefix != FASL_PREFIX:
+        raise Exception("unrecognized prefix")
+
+    shared_count = read_fasl_integer_stream(stream)
+    shared = [None]*shared_count
+
+    length = read_fasl_integer_stream(stream)
+    # read the entire thing and work with a byte-string and a position
+    fasl_string = stream.read(length)
+    pos = 0
+    sexp, pos = fasl_to_sexp_recursive(fasl_string, pos)
+    return sexp
+
+# let's not worry about the CPS'in this right now
+# we probably won't have any sexp deeper than the stack anyways
+def fasl_to_sexp_recursive(fasl_string, pos):
+    #from pycket.interpreter import *
+    from pycket.values import to_list, W_Symbol, W_Fixnum
+
+    typ, pos = read_byte_no_eof(fasl_string, pos)
+
+    if typ == FASL_SYMBOL_TYPE:
+        sym_len, pos = read_fasl_integer(fasl_string, pos)
+        sym_str, pos = read_fasl_string(fasl_string, pos, sym_len)
+        return W_Symbol.make(sym_str), pos
+    elif typ == FASL_LIST_TYPE:
+        list_len, pos = read_fasl_integer(fasl_string, pos)
+        lst_chunk = fasl_string[pos:pos+list_len]
+        lst = [None]*list_len
+        for i in range(list_len):
+            element, pos = fasl_to_sexp_recursive(fasl_string, pos)
+            lst[i] = element
+        return to_list(lst), pos
+    else:
+        if typ >= FASL_SMALL_INTEGER_START:
+            return W_Fixnum((typ-FASL_SMALL_INTEGER_START)+FASL_LOWEST_SMALL_INTEGER), pos
+        else:
+            raise Exception("unrecognized fasl tag : %s" % typ)
+
+def read_fasl_string(fasl_string, pos, length):
+    return read_bytes_exactly(fasl_string, pos, length)
+    # TODO: utf-8
+
+def read_byte_no_eof(fasl_string, pos):
+    return ord(fasl_string[pos]), pos+1
+
+def read_byte_no_eof_stream(stream):
+    b = stream.read(1)
+    if not b:
+        raise Exception("truncated stream - got eof")
+    return b
+
+def read_bytes_exactly(fasl_string, pos, n):
+    if pos+n > len(fasl_string):
+        raise Exception("truncated stream")
+    return fasl_string[pos:pos+n], pos+n
+
+def fasl_integer_inner(b):
+    if b <= 127:
+        return b
+    elif b >= 132:
+        raise Exception("NYI")
+
+def read_fasl_integer(fasl_string, pos):
+    b, new_pos = read_byte_no_eof(fasl_string, pos)
+    return fasl_integer_inner(b), new_pos
+
+def read_fasl_integer_stream(stream):
+    b = ord(read_byte_no_eof_stream(stream))
+    return fasl_integer_inner(b)
